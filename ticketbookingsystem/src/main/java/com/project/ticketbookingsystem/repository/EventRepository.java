283050bed5043074ceaa707018e083f3a905bb64
Fares-alnajjar <your_email@example.com>
2026-04-29 04:07:19 +0300

    final model/repo write what you need in repo

--- ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/repository/EventRepository.java
+++ ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/repository/EventRepository.java
@@ -1,4 +1,10 @@
 package com.project.ticketbookingsystem.repository;
 
-public interface EventRepository {
+import com.project.ticketbookingsystem.model.EventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository  extends JpaRepository<EventEntity,Long> {
+
 }
