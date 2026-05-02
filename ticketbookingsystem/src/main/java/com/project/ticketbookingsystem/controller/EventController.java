9ebfd2f2270eb2900b0c16e5a8a53624aa732999
amr <amrsaidragab@gmail.com>
2026-05-02 22:04:15 +0300

    Revert "added booking page"
    This reverts commit 5b2adf59a95e412f622bb3f58866d22e228f8841.

--- ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/controller/EventController.java
+++ ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/controller/EventController.java
@@ -1,11 +1,9 @@
 package com.project.ticketbookingsystem.controller;
 
-import com.project.ticketbookingsystem.model.EventEntity;
 import com.project.ticketbookingsystem.service.EventService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,12 +20,5 @@ public class EventController {
     public String showEvents(Model model) {
         model.addAttribute("events", eventService.getAllEvents());
         return "events";
-    }
-
-    @GetMapping("/booking/{id}")
-    public String showBooking(@PathVariable Long id, Model model) {
-        EventEntity event = eventService.getEventById(id);
-        model.addAttribute("event", event);
-        return "Booking";
     }
 }
