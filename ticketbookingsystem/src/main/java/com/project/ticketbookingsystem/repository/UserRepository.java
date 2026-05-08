a7079344b6926fe648cd8dc6b9e126dd6b37fe27
Fares-alnajjar <your_email@example.com>
2026-05-08 20:54:57 +0300

    Add signup logic

--- ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/repository/UserRepository.java
+++ ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.project.ticketbookingsystem.repository;
 
 import com.project.ticketbookingsystem.model.UserEntity;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
     boolean existsByEmail(String email);
-    boolean existsByNationalId(String nationalId);
+    boolean existsByNationalId(Long nationalId);
     boolean existsByPhoneNumber(Long phoneNumber);
 }
--- ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/service/SignUpService.java
+++ ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/service/SignUpService.java
@@ -1,5 +1,7 @@
 package com.project.ticketbookingsystem.service;
 
+import com.project.ticketbookingsystem.dto.SignUpDto;
+import com.project.ticketbookingsystem.model.UserEntity;
 import com.project.ticketbookingsystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,4 +14,24 @@ public class SignUpService {
         this.userRepository  = userRepository;
     }
 
+    public UserEntity rsgister(SignUpDto request){
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new IllegalArgumentException("Email already in use");
+        }
+        if(userRepository.existsByNationalId(request.getNationalId())){
+            throw new IllegalArgumentException("National Id already in use");
+        }
+        if(userRepository.existsByPhoneNumber(request.getPhoneNumber())){
+            throw new IllegalArgumentException("Phone Number already in use");
+        }
+
+        UserEntity usere = UserEntity.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .nationalId(request.getNationalId())
+                .password(request.getPassword()) // must be encoded
+                .phoneNumber(request.getPhoneNumber())
+                .build();
+        return userRepository.save(usere) ;
+    }
 }
