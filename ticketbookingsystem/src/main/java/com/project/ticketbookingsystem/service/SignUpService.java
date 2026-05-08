71adab554a5b972b04acd497e0625b3bdb65a758
Omar <omarahmedkamal2006@gmail.com>
2026-05-08 22:50:04 +0300

    view , update , delete user

--- ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/service/SignUpService.java
+++ ticketbookingsystem/src/main/java/com/project/ticketbookingsystem/service/SignUpService.java
@@ -1,10 +1,13 @@
 package com.project.ticketbookingsystem.service;
 
 import com.project.ticketbookingsystem.dto.SignUpDto;
+
 import com.project.ticketbookingsystem.model.UserEntity;
 import com.project.ticketbookingsystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SignUpService {
@@ -33,5 +36,30 @@ public class SignUpService {
                 .phoneNumber(request.getPhoneNumber())
                 .build();
         return userRepository.save(usere) ;
+
     }
+    public void updateUser(UserEntity User) {
+        userRepository.save(User);
+    }
+
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    // used to tell how many Users in admin page
+    public long getTotalUsers() {
+        return userRepository.count();
+    }
+
+    // to put users in dashboard
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public UserEntity getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + id));
+    }
+
 }
